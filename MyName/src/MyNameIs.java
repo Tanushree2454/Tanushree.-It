69015d45ed41dc77dce7b60a7907633b47097e33
Tanushree2454 <130269480+Tanushree2454@users.noreply.github.com>
2023-09-03 17:41:45 +0530

    Add files via upload

---
+++ MyName/src/MyNameIs.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class MyNameIs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter your name ");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+System.out.println("MY name is "+name);
+}
+
+}
